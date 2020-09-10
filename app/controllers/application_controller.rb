0f52d948901129d991b4ade3fa0d78466c490d2d
Eduardo <jeduardo824@gmail.com>
2020-09-10 00:33:39 -0300

    create Companies controller to create, list and consult companies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def render_not_found_response
+    render json: { message: "Record not found" }, status: :not_found
+  end
 end
