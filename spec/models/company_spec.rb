b57f44f01a7430a3163cabb58124b788a0493b0f
Eduardo <jeduardo824@gmail.com>
2020-09-11 08:33:23 -0300

    add has many relation to the Company model

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -4,4 +4,6 @@ require "rails_helper"
 
 RSpec.describe Company, type: :model do
   it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to have_many(:collaborators) }
+  it { is_expected.to accept_nested_attributes_for(:collaborators) }
 end
