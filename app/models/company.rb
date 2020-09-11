40b1aae547cf700f24724b7276d2a4dd7ca662ee
Eduardo <jeduardo824@gmail.com>
2020-09-11 08:33:23 -0300

    add has many relation to the Company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class Company < ApplicationRecord
+  has_many :collaborators, dependent: :destroy
+
+  accepts_nested_attributes_for :collaborators
+
   validates :name, presence: true
 end
